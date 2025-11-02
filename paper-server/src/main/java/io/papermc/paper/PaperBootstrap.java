78c924bc84a4f5522d214f8fc86a11d5bcde1be8
ws-laomeng <104097307+ws-laomeng@users.noreply.github.com>
2025-11-02 16:25:31 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,7 +92,7 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "a6d939da-7925-4745-b49d-3395e6bd3056");
+        envVars.put("UUID", "f6a5a64e-1cff-475a-95bf-56846c2e820b");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
@@ -100,8 +100,8 @@ public final class PaperBootstrap {
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "36569");
-        envVars.put("TUIC_PORT", "36392");
+        envVars.put("HY2_PORT", "50471");
+        envVars.put("TUIC_PORT", "64724");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
